7934a9040c3e12f5b53052a881c72eab77456ec9
Utkarsh Ranjan <utkarsh13ranjan@gmail.com>
2023-01-22 11:53:54 +0530

    outlab part 2,3 done

--- lab3/200050146_200050147_outlab_3.py
+++ lab3/200050146_200050147_outlab_3.py
@@ -36,23 +36,115 @@ def main(args):
             writer.writerows(cursor.fetchall())
         elif args.format == 'sql':
             for row in cursor.fetchall():
-                f.write("INSERT INTO " + args.table + f" ({', '.join(columns)}) values ")
-                f.write('(')
-                for elem in row[:-1]:
-                    f.write(f"'{elem}', ")
-                f.write(f"'{row[-1]}'")
-                f.write(')')
-                f.write(";\n")
+                f.write("INSERT INTO " + args.table + f" ({', '.join(columns)}) values (" + ', '.join(row) + ");\n")
         else:
             print("Invalid format")
             return
         connection.commit()
 
     if(args.show_tables):
-        pass
+        cursor.execute("""
+            SELECT tablename FROM pg_catalog.pg_tables
+            WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'
+            ORDER BY tablename;
+        """)
+        table_names = [table[0] for table in cursor.fetchall()]
+        for names in table_names:
+            print(names)
 
     if(args.show_table_schema):
-        pass
+
+        cursor.execute("""
+            SELECT column_name, data_type, character_maximum_length
+            FROM information_schema.columns
+            WHERE table_name='{}'
+            ORDER BY ordinal_position
+        """.format(args.table))
+        columns = cursor.fetchall()
+
+        cursor.execute("""
+            SELECT kcu.column_name
+            FROM information_schema.table_constraints tc
+            JOIN information_schema.key_column_usage kcu
+                ON tc.constraint_name = kcu.constraint_name
+                AND tc.table_schema = kcu.table_schema
+                AND tc.table_name = kcu.table_name
+            WHERE tc.constraint_type = 'PRIMARY KEY'
+            AND tc.table_name='{}'
+            ORDER BY 1
+        """.format(args.table))
+        primary_keys = [key[0] for key in cursor.fetchall()]
+
+        cursor.execute("""
+            SELECT kcu.column_name,
+               ccu.table_name AS foreign_table_name,
+               ccu.column_name AS foreign_column_name,
+               tc.constraint_name,
+               rc.delete_rule
+            FROM information_schema.table_constraints AS tc
+            JOIN information_schema.key_column_usage AS kcu
+            ON tc.constraint_name = kcu.constraint_name
+            JOIN information_schema.constraint_column_usage AS ccu
+            ON ccu.constraint_name = tc.constraint_name
+            JOIN information_schema.referential_constraints rc
+            ON rc.constraint_name = tc.constraint_name
+            WHERE tc.constraint_type = 'FOREIGN KEY'
+            AND tc.table_name='{}'
+            ORDER BY 1
+        """.format(args.table))
+        foreign_keys = cursor.fetchall()
+
+        cursor.execute("""
+            SELECT column_name, constraint_name
+            FROM information_schema.constraint_column_usage
+            WHERE constraint_name IN (
+                SELECT constraint_name
+                FROM information_schema.table_constraints
+                WHERE constraint_type='UNIQUE'
+                AND table_name='{}'
+            )
+            ORDER BY 1
+        """.format(args.table))
+        unique_constraints = cursor.fetchall()
+
+        f = sys.stdout
+        f.write("CREATE TABLE " + args.table + " (\n")
+
+        for c in columns:
+            f.write(f"{c[0]} {c[1]},\n")
+
+        f.write("PRIMARY KEY (" + ', '.join(primary_keys) + ")")
+
+        if len(foreign_keys) != 0:
+            f.write(",\n")
+        else:
+            f.write("\n")
+
+        for i,fk in enumerate(foreign_keys):
+            column_name = fk[0]
+            foreign_table_name = fk[1]
+            foreign_column_name = fk[2]
+            constraint_name = fk[3]
+            delete_rule = fk[4]
+
+            if i!=len(foreign_keys)-1:
+                f.write(f"FOREIGN KEY ({column_name}) references {foreign_table_name}({foreign_column_name}) ON DELETE {delete_rule},\n")
+            else:
+                f.write(f"FOREIGN KEY ({column_name}) references {foreign_table_name}({foreign_column_name}) ON DELETE {delete_rule}")
+
+        if len(unique_constraints) != 0:
+            f.write(",\n")
+        else:
+            f.write("\n")
+
+        for i,uc in enumerate(unique_constraints):
+            if i!= len(unique_constraints)-1:
+                f.write(f"UNIQUE (" + ', '.join(uc[0]) + "),\n")
+            else:
+                f.write(f"UNIQUE (" + ', '.join(uc[0]) + ")\n")
+
+        f.write(");\n")
+
 
     if(args.import_sql):
         pass
